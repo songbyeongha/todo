e2750fca79f913ce218cea0cf7e59334e95f55dd
songbyeongha <qudgk0006@naver.com>
2019-11-11 22:41:12 +0900

    Add module 2

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,28 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as getters from './getters';
-import * as mutations from './mutations';
+import todoApp from './modules/todoApp'
+// import * as getters from './getters';
+// import * as mutations from './mutations';
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    }
-}
-
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch()
-    },
-    getters,
-    mutations
+    modules: {
+        todoApp
+    }
 });
